41d6ceebb058d360fa035c666769199c851d4da1
Zeus <hidarizeus@gmail.com>
2022-10-14 14:45:25 +0800

    ff

--- lib/extension/extension.dart
+++ lib/extension/extension.dart
@@ -1,9 +1,8 @@
 import 'package:m3u_z_parser/src/models/m3u_entry.dart';
 
 extension SORTER on List<M3uEntry> {
-  Map<String, List<M3uEntry>> categorize(
-      {required List<M3uEntry> data, required String needle}) {
-    return data.fold(<String, List<M3uEntry>>{}, (acc, current) {
+  Map<String, List<M3uEntry>> categorize({required String needle}) {
+    return fold(<String, List<M3uEntry>>{}, (acc, current) {
       final property = current.attributes[needle] ?? "tvg-id";
 
       if (!acc.containsKey(property)) {
